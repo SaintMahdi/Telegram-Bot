8881236b01143dc27ba75fc17966c9ac2ccf0b70
ROG <ROG@DESKTOP-4HJL82Q>
2023-11-01 10:13:39 +0330

    Add project files.

---
+++ Telegram Bot/IMDB.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using JMovies.IMDb.Entities.Movies;
+using JMovies.IMDb.Providers;
+using JMovies.IMDb.Entities.Settings;
+using JMovies.IMDb.Entities.Interfaces;
+using JMovies.IMDb.Entities.Settings.Presets;
+using Telegram.Bot.Requests.Abstractions;
+
+namespace Telegram_Bot
+{
+    //internal class imdb
+    //{
+    //    IIMDbDataProvider imdbDataProvider = new IMDbScraperDataProvider();
+
+    //        Movie movie = imdbDataProvider.GetMovie(232500, new BasicProductionDataFetchSettings
+    //        {
+    //            PreferredCulture = "tr-TR"
+    //        }); // https://www.imdb.com/title/tt0232500/
+
+    //}
+}
